33deeb09529c84e65fa5c19cbd87f90fee798b88
Borat <Battlefielder21@yahoo.co.uk>
2014-06-18 12:59:50 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,11 @@
-import unittest
+#Currency
+GBP = 1
+EUR = 2
+USD = 5
+JPY = 4
+Currency = raw_imput("Currency you want to change from (GBP, EUR, USD, JPY?")
+if Currency.isalpha:
+    print "You did not input a correct Currency"
+    break
+Ammount = raw_input("Ammount of currency you want to convert e.g 3.75")
+to = raw_imput("Currency converting to?")
