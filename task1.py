dc5d425775762ad54e41a3a8dbbf34fac9843c58
BORAT <battlefielder21@yahoo.co.uk>
2014-06-20 14:11:06 +0000

    conversion to EUR from GBP working

--- task1.py
+++ task1.py
@@ -1,3 +1,6 @@
 currs = ['GBP', 'EUR', 'USD', 'JPY']
 rates = [1,1.2,1.3,1.4] # startup rates
 
+def conv(money, cFrom, cTo):
+    answer = money *rates[1]
+    return (answer)
--- test1.py
+++ test1.py
@@ -10,5 +10,9 @@ class TestSequenceFunctions(unittest.TestCase):
     def test1(self):
         assert t1.rates[0] == 1# test 1 fails rate for GBP not 1
         
+    def test2(self):
+        assert (t1.rates[1]==2)#rate for euros should be 2
+        assert (t1.conv(10,0,1)== 20)# 10 GBP(curr0) should make 20 eur(currency1)
+        
 if __name__ =='__main__':
     unittest.main()
