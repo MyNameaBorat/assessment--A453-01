4e03e719014d2c7201dcceb1dc9f1daaea2e3614
BORAT <battlefielder21@yahoo.co.uk>
2014-06-20 14:16:13 +0000

    3 tests working

--- task1.py
+++ task1.py
@@ -2,5 +2,6 @@ currs = ['GBP', 'EUR', 'USD', 'JPY']
 rates = [1,1.2,1.3,1.4] # startup rates
 
 def conv(money, cFrom, cTo):
-    answer = money *rates[1]
+    answer = money *rates[cTo]
+    answer = answer /rates[cFrom]
     return (answer)
--- test1.py
+++ test1.py
@@ -13,6 +13,13 @@ class TestSequenceFunctions(unittest.TestCase):
     def test2(self):
         assert (t1.rates[1]==2)#rate for euros should be 2
         assert (t1.conv(10,0,1)== 20)# 10 GBP(curr0) should make 20 eur(currency1)
+    
+    def test3(self):
+        assert (t1.conv(20,1,0) == 10)# 20 euros should make 10 pounds
+        
+    def test4(self):
+        assert(t1.conv(20,1,3)==40)#20eur should make JPY40
+        
         
 if __name__ =='__main__':
     unittest.main()
